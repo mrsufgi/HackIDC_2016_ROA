42617b07a52951aa83d3996dfce1cbbd3f64453f
Aviad Hahami <aviadhahami@gmail.com>
2016-05-06 22:42:01 +0300

    fixed fa

--- client/src/components/RoastCardActions.jsx
+++ client/src/components/RoastCardActions.jsx
@@ -1,16 +1,24 @@
 var React = require('react');
 import {Button} from 'react-bootstrap';
+var FontAwesome = require('react-fontawesome');
 
 var RoastCardActions = React.createClass({
+	propTypes: {
+		id: React.PropTypes.string
+	},
 	render() {
 		return (
 			<table style={{width: '100%'}}>
 				<tr>
-					<td style={{width: '50%;'}}>
-						<Button style={{width: '100%'}}>Test</Button>
+					<td style={{width: '50%'}}>
+						<Button style={{width: '90%'}} bsStyle='danger'>
+							<FontAwesome name='thumbs-o-down'/>
+						</Button>
 					</td>
-					<td style={{width: '50%;'}}>
-						<Button style={{width: '100%'}}>Test</Button>
+					<td style={{width: '50%'}}>
+						<Button style={{width: '95%'}} bsStyle='success'>
+							<FontAwesome name='thumbs-o-up' />
+						</Button>
 					</td>
 				</tr>
 			</table>
