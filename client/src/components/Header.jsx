9f45f1ad611c95732880e364e9a6340638d38b10
Aviad Hahami <aviadhahami@gmail.com>
2016-05-06 17:13:37 +0300

    added nav...not useful

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
 var React = require('react');
 var Header = React.createClass({
 	style: {
@@ -12,56 +12,29 @@ var Header = React.createClass({
 	},
 	render() {
 		return (
-			<Nav className='navbar navbar-default'>
-				<div className='container-fluid'>
-					<div className='navbar-header'>
-						<button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
-							<span className='sr-only'>Toggle navigation</span>
-							<span className='icon-bar'></span>
-							<span className='icon-bar'></span>
-							<span className='icon-bar'></span>
-						</button>
-						<a className='navbar-brand' href='#'>Brand</a>
-					</div>
-					<div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
-						<ul className='nav navbar-nav'>
-							<li className='active'><a href='#'>Link <span className='sr-only'>(current)</span></a></li>
-							<li><a href='#'>Link</a></li>
-							<li className='dropdown'>
-								<a href='#' className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Dropdown <span className='caret'></span></a>
-								<ul className='dropdown-menu'>
-									<li><a href='#'>Action</a></li>
-									<li><a href='#'>Another action</a></li>
-									<li><a href='#'>Something else here</a></li>
-									<li role='separator' className='divider'></li>
-									<li><a href='#'>Separated link</a></li>
-									<li role='separator' className='divider'></li>
-									<li><a href='#'>One more separated link</a></li>
-								</ul>
-							</li>
-						</ul>
-						<form className='navbar-form navbar-left' role='search'>
-							<div className='form-group'>
-								<input type='text' className='form-control' placeholder='Search' />
-							</div>
-							<button type='submit' className='btn btn-default'>Submit</button>
-						</form>
-						<ul className='nav navbar-nav navbar-right'>
-							<li><a href='#'>Link</a></li>
-							<li className='dropdown'>
-								<a href='#' className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Dropdown <span className='caret'></span></a>
-								<ul className='dropdown-menu'>
-									<li><a href='#'>Action</a></li>
-									<li><a href='#'>Another action</a></li>
-									<li><a href='#'>Something else here</a></li>
-									<li role='separator' className='divider'></li>
-									<li><a href='#'>Separated link</a></li>
-								</ul>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</Nav>
+			<Navbar inverse>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<a href='#'>RoastMe!</a>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav>
+						<NavItem eventKey={1} href='#'>Feed</NavItem>
+						<NavItem eventKey={2} href='#'>Link</NavItem>
+					</Nav>
+					<Nav pullRight>
+						<NavDropdown eventKey={3} title='Profile' id='basic-nav-dropdown'>
+							<MenuItem eventKey={3.1}>Me</MenuItem>
+							<MenuItem eventKey={3.2}>Edit Profile</MenuItem>
+							<MenuItem eventKey={3.3}></MenuItem>
+							<MenuItem divider />
+							<MenuItem eventKey={3.3}>Sign Out</MenuItem>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
 		);
 	}
 });
