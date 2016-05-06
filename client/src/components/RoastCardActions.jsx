fc50bebb51fba1eca0d2fc5b31dc365a27e4aa9b
Aviad Hahami <aviadhahami@gmail.com>
2016-05-07 02:49:36 +0300

    fixed getter

--- client/src/components/RoastCardActions.jsx
+++ client/src/components/RoastCardActions.jsx
@@ -6,18 +6,59 @@ var RoastCardActions = React.createClass({
 	propTypes: {
 		id: React.PropTypes.string
 	},
+	like: function() {
+		if (this.state.votedD) {
+			this.setState({
+				votedD: false,
+				votedL: true,
+				dislike: this.state.dislike - 1,
+				like: this.state.like + 1
+			});
+		} else if (!this.state.votedD && !this.state.votedL) {
+			this.setState({
+				votedD: false,
+				votedL: true,
+				like: this.state.like + 1
+			});
+		}
+		console.log('like');
+	},
+	dislike: function() {
+		if (this.state.votedL) {
+			this.setState({
+				votedD: true,
+				votedL: false,
+				dislike: this.state.dislike + 1,
+				like: this.state.like - 1
+			});
+		} else if (!this.state.votedD && !this.state.votedL) {
+			this.setState({
+				votedD: true,
+				votedL: false,
+				dislike: this.state.like + 1
+			});
+		}
+		console.log('dislike');
+	},
+	getInitialState: function() {
+		return ({dislike: 0, like: 0, votedL: false, votedD: false});
+	},
 	render() {
 		return (
 			<table style={{width: '100%'}}>
 				<tr>
 					<td style={{width: '50%'}}>
-						<Button style={{width: '90%', margin: 'auto'}} bsStyle='success'>
-							<FontAwesome name='thumbs-o-up' />
+						<Button style={{width: '90%', margin: 'auto'}} bsStyle='success' onClick={this.like}>
+							<div>
+								{this.state.like} <FontAwesome name='thumbs-o-up' />
+							</div>
 						</Button>
 					</td>
 					<td style={{width: '50%'}}>
-						<Button style={{width: '90%', margin: 'auto'}} bsStyle='danger'>
-							<FontAwesome name='thumbs-o-down'/>
+						<Button style={{width: '90%', margin: 'auto'}} bsStyle='danger' onClick={this.dislike}>
+							<div>
+								{this.state.dislike} <FontAwesome name='thumbs-o-down'/>
+							</div>
 						</Button>
 					</td>
 				</tr>
