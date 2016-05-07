0eb7a2e4c9871e776d20f95889e75c96aa2f7ab0
Aviad Hahami <aviadhahami@gmail.com>
2016-05-07 04:30:38 +0300

    nice

--- client/src/components/RoastCard.jsx
+++ client/src/components/RoastCard.jsx
@@ -4,6 +4,7 @@ var CommentBox = require('../components/CommentBox.jsx');
 var CustomPanelHeader = require('../components/CustomPanelHeader.jsx');
 var RoastCardActions = require('../components/RoastCardActions.jsx');
 var SeeMore = require('../components/SeeMore.jsx');
+import {Link} from 'react-router';
 import {Panel} from 'react-bootstrap';
 
 var RoastCard = React.createClass({
@@ -11,13 +12,16 @@ var RoastCard = React.createClass({
 		cardData: React.PropTypes.object
 	},
 	render() {
+		var location = '/' + this.props.cardData.id;
 		return (
 			<div className='roast-card shadow'>
 				<Panel header={<CustomPanelHeader title={this.props.cardData.title} user={this.props.cardData.name}/>} bsStyle='primary' footer={<RoastCardActions id={this.props.cardData.id} />}>
 					<div>
 						<RoastCardPic pic={this.props.cardData.roastPicSrc}></RoastCardPic>
 						<CommentBox comments={this.props.cardData.comments}></CommentBox>
-						<SeeMore />
+						<Link to={location}>
+							<SeeMore />
+						</Link>
 					</div>
 				</Panel>
 
