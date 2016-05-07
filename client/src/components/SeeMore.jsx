ed6143d609bc2c2105081d7a3b27ce0947e83296
Aviad Hahami <aviadhahami@gmail.com>
2016-05-07 04:25:02 +0300

    nice

--- client/src/components/SeeMore.jsx
+++ client/src/components/SeeMore.jsx
@@ -4,7 +4,7 @@ import {Button} from 'react-bootstrap';
 var SeeMore = React.createClass({
 	render() {
 		return (
-			<Button className='see-more' bsStyle='info'>see more</Button>
+			<Button className='see-more no-outline' bsStyle='info'>see more</Button>
 		);
 	}
 });
