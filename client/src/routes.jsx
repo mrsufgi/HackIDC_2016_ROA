66e65e7be69958c9dceba47e5c7522e5cf5bc11b
Aviad Hahami <aviadhahami@gmail.com>
2016-05-07 13:01:47 +0300

    fixed route

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Route from 'react-router/lib/Route';
+import {Route} from 'react-router';
 
 import App from './routes/App';
 import Feed from './components/Feed.jsx';
@@ -9,7 +9,7 @@ import SignUp from './components/login/SignUp.jsx';
 
 export default (
 	<Route component={App}>
-		<Route path='/' component={Feed} />
+		<Route path='/' component={Feed}/>
 		<Route path='/profile' component={Profile} />
 		<Route path='/signin' component={SignIn} />
 		<Route path='/signup' component={SignUp} />
